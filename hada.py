08e7cb3e48c91e60ac775f04b734123c312ad54a
IvanShchupko <128311879+IvanShchupko@users.noreply.github.com>
2025-11-21 11:44:45 +0100

    Improve snake game with file compression and save
    Refactor snake game logic and add compression feature

--- hada.py
+++ hada.py
@@ -1,9 +1,46 @@
+""" subor = open('hada.txt', 'r')
+subor2 = open('hada kompresia.txt', 'w')
+pocet = 0
+max = 0
+def komresia(s):
+    if s == "":
+        return
+    s = s + '.'
+    pismeno = s[0]
+    pocet = 0
+    vystup = 0
+    for znak in s:
+        if znak == pismeno:
+            pocet += 1
+        else:
+            vystup = vystup + "{} {}".format(pismeno, pocet)
+            pismeno = znak
+            pocet = 1
+        return vystup
+    
+for riadok in subor:
+    riadok = riadok.strip()
+    print(riadok)
+    riadok2 = komresia(riadok)
+    subor2.write(riadok2)
+    print(riadok2)
+    if len(riadok) > max:
+        max = len(riadok)
+    pocet += 1
+
+komresia()
+
+print('pocet riadkov v subore: ' + pocet)
+print('pocet krokov v najdlhsej hre: ' + max)
+subor.close()
+subor2.close() """
+
 import tkinter
 canvas = tkinter.Canvas(width=400, height=400)
 canvas.pack()
 
 def posun():
-    global xhlava, yhlava, narazil, sx, sy
+    global xhlava, yhlava, narazil, chod, smer
     xhlava += sx
     yhlava  += sy
     z = canvas.coords('hada')
@@ -17,25 +54,43 @@ def posun():
     if not narazil:
         z.append(xhlava)
         z.append(yhlava)
+        chod = chod + smer
         canvas.coords('hada', z)
         canvas.after(10, posun)
 
 def klaves(event):
-    global sx, sy
+    global sx, sy, smer
     if event.keysym == 'Left':
-        sx, sy == -1, 0
+        sx, sy = -1, 0
+        smer = 'L'
     if event.keysym == 'Right':
-        sx, sy == 1, 0
+        smer = 'R'
+        sx, sy = 1, 0
     if event.keysym == 'Up':
-        sx, sy == 0, -1
+        smer = 'U'
+        sx, sy = 0, -1
     if event.keysym == 'Down':
-        sx, sy == 0, 1
+        smer = 'D'
+        sx, sy = 0, 1
+
+def save():
+    hada = open('hada.txt', 'w')
+    hada.write('\n' + chod)
+    hada.close()
 
 narazil = False
 xhlava, yhlava = 200, 200
 sx, sy = 0, -1
+smer = 'H'
+chod = ''
+
+
 canvas.create_line(xhlava,yhlava-sy, xhlava, yhlava, tags='hada')
 posun()
+
+button1 = tkinter.Button(text='save', command=save)
+button1.pack()
+
 canvas.bind_all('<Key>', klaves)
 
 canvas.mainloop()
